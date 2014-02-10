d63743411c0bc0e161045eea01f9a9e5bbd82ac2
Peter Kosyh <p.kosyh@gmail.com>
2014-02-10 13:03:10 +0400

    i18n fix

--- i18n.lua
+++ i18n.lua
@@ -20,6 +20,9 @@ _ = function(s)
 	local l = LANG
 	local a = s:gsub("^([^:]+):.*$", "%1")
 	s = s:gsub("^[^:]+:", "")
+	if not l or not i18[l] then
+		return s
+	end
 	local ss = i18[l][a] 
 	if ss then return ss end
 	return s
